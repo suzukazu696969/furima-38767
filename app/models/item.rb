a43d1c576c188afa6dd02b671c705eb3516532d4
suzukazu696969 <rugurethureira@gmail.com>
2022-12-23 17:41:09 +0900

    モデル作成しActivehash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user, :category, :commodity_condition, :shipping_charge, :region_of_origin, :days_to_ship
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  validates :title, presence: true
+  validates :delivery, presence: true
+  validates :user, presence: true
+  validates :commodity_condition_id, numericality: { other_than: 1 } 
+  validates :shipping_charge_id, numericality: { other_than: 1 } 
+  validates :region_of_origin_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 }  
 end
--- db/migrate/20221223054307_create_items.rb
+++ db/migrate/20221223054307_create_items.rb
@@ -5,6 +5,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :price, null: false
       t.text :delivery, null: false
       t.references :user, null: false, foreign_key: true
+      t.integer :commodity_condition_id, null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :region_of_origin_id, null: false
+      t.integer :days_to_ship_id, null: false
+      t.integer :category_id, null: false
       t.timestamps
     end
   end
