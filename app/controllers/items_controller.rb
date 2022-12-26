9c6f19d7be1432f46f4f0223757846f8f4d11703
suzukazu696969 <rugurethureira@gmail.com>
2022-12-26 12:56:10 +0900

    画像保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     if  @item.save
       redirect_to root_path
     else
@@ -22,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :delivery, :commodity_condition_id, :shipping_charge_id, :region_of_origin_id, :days_to_ship_id, :category_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :image, :price, :delivery, :commodity_condition_id, :shipping_charge_id, :region_of_origin_id, :days_to_ship_id, :category_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-   belongs_to_active_hash :category, :commodity_condition, :shipping_charge, :region_of_origin, :days_to_ship
+   belongs_to_active_hash :category
+   belongs_to_active_hash :commodity_condition
+   belongs_to_active_hash :shipping_charge
+   belongs_to_active_hash :region_of_origin
+   belongs_to_active_hash :days_to_ship 
   has_one_attached :image
   belongs_to :user
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
   validates :title, presence: true
+  validates :image, presence: true
   validates :delivery, presence: true
   validates :user, presence: true
   validates :commodity_condition_id, numericality: { other_than: 1 } 
