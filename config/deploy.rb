995ba1a7353c5d0b8724b3c913e2ab36bb3b9a38
suzukazu696969 <rugurethureira@gmail.com>
2023-01-17 12:33:32 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "3.17.1"
 
 set :application, "furima_38767"
 set :repo_url,  'git@github.com:suzukazu696969/furima-38767.git'
@@ -7,7 +7,7 @@ set :repo_url,  'git@github.com:suzukazu696969/furima-38767.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, ' 2.6.5 ' 
+set :rbenv_ruby, '2.6.5' 
 
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/lindaman.pem'] 
